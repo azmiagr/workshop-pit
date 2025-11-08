6c33e3527502dc8094c96f8502919ad9c8309da5
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-11-08 16:56:12 +0700

    feat: add User entity with fields for user management

---
+++ entity/user.go
@@ -0,0 +1,19 @@
+package entity
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	UserID      uuid.UUID `json:"user_id" gorm:"type:varchar(36);primaryKey"`
+	Name        string    `json:"name" gorm:"type:varchar(255);not null"`
+	Email       string    `json:"email" gorm:"type:varchar(255);unique;not null"`
+	Password    string    `json:"password" gorm:"type:varchar(255);not null"`
+	PhoneNumber string    `json:"phone_number" gorm:"type:varchar(255);unique;not null"`
+	CreatedAt   time.Time `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt   time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+
+	Loans []Loan `json:"loans" gorm:"foreignKey:UserID;"`
+}
