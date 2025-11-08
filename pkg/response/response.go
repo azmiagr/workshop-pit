981594cf7f2efc756ce23176a2f19ffed3507711
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-11-08 16:58:34 +0700

    feat: add response package with success and error handling functions for API responses

---
+++ pkg/response/response.go
@@ -0,0 +1,43 @@
+package response
+
+import "github.com/gin-gonic/gin"
+
+type Response struct {
+	Status  Status      `json:"status"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+type Status struct {
+	Code      int  `json:"code"`
+	IsSuccess bool `json:"isSuccess"`
+}
+
+func Success(ctx *gin.Context, code int, message string, data any) {
+	ctx.JSON(code, Response{
+		Status: Status{
+			Code:      code,
+			IsSuccess: true,
+		},
+		Message: message,
+		Data:    data,
+	})
+}
+
+func Error(ctx *gin.Context, code int, message string, err error) {
+	var errorData interface{}
+	if err != nil {
+		errorData = err.Error()
+	} else {
+		errorData = nil
+	}
+
+	ctx.JSON(code, Response{
+		Status: Status{
+			Code:      code,
+			IsSuccess: false,
+		},
+		Message: message,
+		Data:    errorData,
+	})
+}
