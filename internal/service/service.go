904e52cafc5dc4bf92b1d1d7c55df9df218d8aeb
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-11-08 16:57:08 +0700

    feat: create Service struct to initialize BookService for book management

---
+++ internal/service/service.go
@@ -0,0 +1,13 @@
+package service
+
+import "workshop-pit/internal/repository"
+
+type Service struct {
+	BookService IBookService
+}
+
+func NewService(repository *repository.Repository) *Service {
+	return &Service{
+		BookService: NewBookService(repository),
+	}
+}
